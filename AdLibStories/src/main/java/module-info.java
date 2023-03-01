b8857d9af245246b8bcb52d40644bffdcd0b2a33
Michael-Robertson5244 <89548533+Michael-Robertson5244@users.noreply.github.com>
2023-03-01 12:45:09 -0600

    Delete AdLibStories directory

--- AdLibStories/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module AdLibStories.AdLibStories {
-    requires javafx.controls;
-	requires org.mongodb.driver.sync.client;
-	requires org.mongodb.bson;
-	requires org.mongodb.driver.core;
-	requires javafx.graphics;
-    exports AdLibStories.AdLibStories;
-}
