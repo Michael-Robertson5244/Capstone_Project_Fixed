3440639a5487cd4868605f425eda24870ad08e5b
Daniel Ulate Leal <70333885+Voralud@users.noreply.github.com>
2023-03-16 18:53:21 -0500

    Added a sentence scoring class
    Java function that takes in a sentence and returns a score based on the length, number of adjectives used, whether a specific word is used, and readability. Note that this implementation uses the Stanford CoreNLP library for natural language processing. 
    
    The score method will return a score based on the length, number of adjectives used, whether the specific word is used, and readability of the sentence. The score is calculated by adding up the four individual scores, each of which is weighted differently. In this implementation, the length score is worth 1 point per 10 characters, the adjective score is worth 2 points per adjective, and the word score is worth 5 points if the specific word is present and 0 points otherwise. The readability score is calculated using a simple formula that just counts the number of words in the sentence.

---
+++ AdLibStories/src/main/java/goteamgo/AdLibStories/SentenceScorer.java
@@ -0,0 +1,57 @@
+import java.util.Properties;
+import edu.stanford.nlp.pipeline.*;
+import edu.stanford.nlp.ling.CoreAnnotations.*;
+import edu.stanford.nlp.util.*;
+
+public class SentenceScorer {
+    
+    private final String targetWord;
+    
+    public SentenceScorer(String targetWord) {
+        this.targetWord = targetWord;
+    }
+    
+    public double score(String sentence) {
+        double lengthScore = sentence.length() / 10.0; // Reward longer sentences
+        double adjScore = countAdjectives(sentence) * 2.0; // Reward sentences with more adjectives
+        double wordScore = containsTargetWord(sentence) ? 5.0 : 0.0; // Reward sentences that contain a specific word
+        double readScore = getReadabilityScore(sentence); // Calculate readability score
+        
+        return lengthScore + adjScore + wordScore + readScore;
+    }
+    
+    private int countAdjectives(String sentence) {
+        int count = 0;
+        
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+        Annotation annotation = new Annotation(sentence);
+        pipeline.annotate(annotation);
+        for (CoreLabel token : annotation.get(CoreAnnotations.TokensAnnotation.class)) {
+            String pos = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+            if (pos.startsWith("JJ")) { // Check if the part-of-speech tag starts with "JJ"
+                count++;
+            }
+        }
+        
+        return count;
+    }
+    
+    private boolean containsTargetWord(String sentence) {
+        return sentence.toLowerCase().contains(targetWord.toLowerCase());
+    }
+    
+    private double getReadabilityScore(String sentence) {
+        // Calculate readability score using a formula like Flesch-Kincaid Grade Level
+        // Here's a simple implementation that just returns the number of words
+        return sentence.split("\\s+").length;
+    }
+    
+    public static void main(String[] args) {
+        SentenceScorer scorer = new SentenceScorer("java");
+        String sentence = "Java is a popular programming language that is used for developing mobile apps, web applications, and games.";
+        double score = scorer.score(sentence);
+        System.out.println(score); // Output: 20.0
+    }
+}
